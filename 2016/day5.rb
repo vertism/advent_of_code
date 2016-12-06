e08a223cc78ca4d0ab7b5c928c5de432c6f0e1c1
Andrew Pepler <andrew@vertism.com>
2016-12-06 11:03:02 +0000

    Speed up

--- 2016/day5.rb
+++ 2016/day5.rb
@@ -5,31 +5,29 @@ require 'digest'
 class Day5
 
   def run
-    password = []
-    input = 'reyedfim'
+    start = Time.now
+    password1 = []
+    password2 = Array.new(8, nil)
     md5 = Digest::MD5.new
+    input = 'reyedfim'
     i = 0
-    until password.size == 8
+    while true
       hash = md5.hexdigest "#{input}#{i}"
-      i += 1
       if hash[0..4] == "00000"
-        password << hash[5]
+        password1 << hash[5] unless password1.size == 8
+        if ('0'..'7').include?(hash[5]) && password2[hash[5].to_i].nil?
+          password2[hash[5].to_i] = hash[6] unless password2.compact.size == 8
+        end
+        break if password1.size == 8 && password2.compact.size == 8
       end
-    end
-
-    puts "Part 1: #{password.join('')}"
-
-    i = 0
-    password = Array.new(8, nil)
-    until password.compact.size == 8
-      hash = md5.hexdigest "#{input}#{i}"
       i += 1
-      if hash[0..4] == "00000" && ('0'..'7').include?(hash[5]) && password[hash[5].to_i].nil?
-        password[hash[5].to_i] = hash[6]
-      end
     end
 
-    puts "Part 1: #{password.join('')}"
+    puts "Part 1: #{password1.join('')}"
+    puts "Part 2: #{password2.join('')}"
+
+    finish = Time.now
+    puts finish - start
   end
 end
 
