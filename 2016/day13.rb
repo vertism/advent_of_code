0f09ab6317990de5d9038bb2d807df02eebfc9e3
Andrew Pepler <andrew@vertism.com>
2016-12-13 22:48:30 +0000

    Day 13

--- 2016/day13.rb
+++ 2016/day13.rb
@@ -6,49 +6,54 @@ class Day12
 
   def initialize
     @input = 1352
+    @positions = {}
+    @x = 50
+    @y = 50
   end
 
   def run
     create_map
-
-    @positions = {}
-    @steps = 0
-    @current_position = Coord.new(1,1)
-
-    possible_moves.each do |new_position|
-      move(new_position)
+    move(Coord.new(1,1), 0)
+    target = Coord.new(31, 39)
+    
+    puts "Part 1: #{@positions[target]}"
+    puts "Part 2: #{@positions.values.count { |steps| steps <= 50 }}"
+  end
+  
+  def move(new_position, steps)
+    if !@positions.has_key?(new_position) || @positions[new_position] > steps
+      @positions[new_position] = steps
+    else
+      return
     end
-
-    "Part 1: 90"
+    
+    @current_position = new_position
+    
+    possible_moves.each do |new_position|
+      move(new_position, steps+1)
+    end
   end
 
   def possible_moves
     moves = []
-    if @map[@current_position.x, @current_position.y+1] == '.'
+    if @current_position.y < @y-1 && @map[@current_position.x][@current_position.y+1] == ?.
       moves << Coord.new(@current_position.x, @current_position.y+1)
     end
-    if @map[@current_position.x, @current_position.y-1] == '.'
+    if @current_position.y > 0 && @map[@current_position.x][@current_position.y-1] == ?.
       moves << Coord.new(@current_position.x, @current_position.y-1)
     end
-    if @map[@current_position.x+1, @current_position.y] == '.'
+    if @current_position.x < @x-1 && @map[@current_position.x+1][@current_position.y] == ?.
       moves << Coord.new(@current_position.x+1, @current_position.y)
     end
-    if @map[@current_position.x-1, @current_position.y] == '.'
+    if @current_position.x > 0 && @map[@current_position.x-1][@current_position.y] == ?.
       moves << Coord.new(@current_position.x-1, @current_position.y)
     end
     moves
   end
 
-  def move(new_position)
-    @step += 1
-    possible_moves.each do |new_position|
-      @step += move(new_position)
-    end
-  end
-
   def create_map
-    @map = 50.times.map do |i|
-      50.times.map do |j|
+    @map = @x.times.map do |i|
+      @y.times.map do |j|
         type(Coord.new(i, j))
       end
     end
@@ -61,7 +66,7 @@ class Day12
   end
 
   def type(coord)
-    sum = (coord.x * coord.x) + (coord.x * 3) + (2 * coord.x * coord.y) + coord.y + (coord.y * coord.y) + @input
+    sum = (coord.x ** 2) + (coord.x * 3) + (2 * coord.x * coord.y) + coord.y + (coord.y ** 2) + @input
     if sum.to_s(2).chars.count{ |c| c == '1' } % 2 == 0
       '.'
     else
