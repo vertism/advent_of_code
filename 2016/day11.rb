9169abbd13dc7fb65b3425e4ad90128d5e55ef63
Andrew Pepler <andrew@vertism.com>
2016-12-14 17:46:11 +0000

    Update Day11 to use loop instead of recursion

--- 2016/day11.rb
+++ 2016/day11.rb
@@ -1,111 +1,78 @@
 #!/usr/bin/env ruby
 
-# [],
-# [:bm, :cm, :dm, :em],
-# [:bg, :cg, :dg, :eg],
-# [:e, :ag, :am]
-
 class Day11
-  Item = Struct.new(:floor, :type) do
-    def <=>(other)
-      self[:floor] <=> other[:floor]
-    end
-  end
-  
-  def initialize
-    @e = Item.new(1, 'e')
-    @ag = Item.new(1,'ag')
-    @am = Item.new(1, 'am')
-    @bg = Item.new(2, 'bg')
-    @cg = Item.new(2, 'cg')
-    @dg = Item.new(2, 'dg')
-    @eg = Item.new(2, 'eg')
-    @bm = Item.new(3, 'bm')
-    @cm = Item.new(3, 'cm')
-    @dm = Item.new(3, 'dm')
-    @em = Item.new(3, 'em')
-    @finished = false
-    @indentifiers = []
-  end
-  
-  def run
-    until @finished == true
-      steps = try_move(0)
-    end
-    
-    puts "Part 1: #{steps}"
-  end
-  
-  def try_move(steps)
-    on_elevator_floor_groups.each do |objects|
-      [1, -1].each do |i|
-        occupants = [objects].flatten
-        if move(i, occupants)
-          steps += 1
 
-          if all_objects.all? { |o| o[:floor] == 4 }
-            @finished = true
-          else
-            steps += try_move(steps)
+  def run
+    @positions = {}
+    collect('12131-0')
+    puts "Part 1: #{@positions['44444']}"
+    @positions = {}
+    collect('11123232323-0')
+    puts "Part 2: #{@positions['44444444444']}"
+  end
+
+  def collect(start)
+    stack = [start]
+    while !stack.empty?
+      puts stack.size
+      state = stack.delete_at(0)
+      state, steps = state.split('-')
+      next if state == ('4' * state.size)
+      state = state.chars.map(&:to_i)
+      steps = steps.to_i
+
+      groups(state).each do |objects|
+        occupants = *objects
+        directions = if state[0] == 1
+          [1]
+        elsif state[0] == 4
+          [-1]
+        else
+          [1, -1]
+        end
+
+        directions.each do |i|
+          new_state = state.clone
+          if move(i, new_state, occupants, steps)
+            stack << "#{new_state.join('')}-#{steps+1}" unless steps >= 30
           end
         end
       end
     end
-    steps
   end
-  
-  def move(direction, occupants)
-    if ![1, -1].include?(direction)
-      return false
-    end
-    
-    occupants.each { |o| o[:floor] += direction }
-    @e[:floor] += direction
-    
-    if valid?(direction)
-      @indentifiers << identifier
-      true
-    else
-      move(direction * -1, occupants)
-      false
-    end
+
+  def groups(state)
+    elevator = state[0]
+    available = state.each_index.select{|i| state[i] == elevator }[1..-1]
+    available + available.permutation(2).map(&:sort).uniq
   end
-  
-  def valid?(direction)
-    return false if @indentifiers.index(identifier)
-    
-    if @e[:floor] == 4 && direction == 1 || @e[:floor] == 1 && direction == -1
-      return false
+
+  def move(direction, state, occupants, steps)
+    ([0] + occupants).each { |i| state[i] += direction }
+
+    if valid = valid?(state)
+      identifier = state.join('')
+      if !@positions[identifier] || @positions[identifier] > steps+1
+        @positions[identifier] = steps+1
+      else
+        valid = false
+      end
     end
-    
-    if (@ag[:floor] != @am[:floor]) && [@bg[:floor], @cg[:floor], @dg[:floor], @eg[:floor]].include?(@am[:floor])
-      return false
-    elsif (@bg[:floor] != @bm[:floor]) && [@ag[:floor], @cg[:floor], @dg[:floor], @eg[:floor]].include?(@bm[:floor])
-      return false
-    elsif (@cg[:floor] != @cm[:floor]) && [@ag[:floor], @bg[:floor], @dg[:floor], @eg[:floor]].include?(@cm[:floor])
-      return false
-    elsif (@dg[:floor] != @dm[:floor]) && [@ag[:floor], @bg[:floor], @cg[:floor], @eg[:floor]].include?(@dm[:floor])
-      return false
-    elsif (@eg[:floor] != @em[:floor]) && [@ag[:floor], @bg[:floor], @cg[:floor], @dg[:floor]].include?(@em[:floor])
-      return false
-    end
-    
+
+    valid
+  end
+
+  def reset(direction, state, occupants)
+    ([0] + occupants).each { |i| state[i] += direction }
+  end
+
+  def valid?(state)
+    return false if state[0] < 1 || state[0] > 4
+    return false if (state[1] != state[2]) && (state[3] == state[2])
+    return false if (state[3] != state[4]) && (state[1] == state[4])
     true
   end
-  
-  def on_elevator_floor_groups
-    available = all_objects.select { |o| o.floor == @e.floor }
-    available + available.permutation(2).to_a.sort.uniq
-  end
-  
-  def all_objects
-    [@ag, @am, @bg, @cg, @dg, @eg, @bm, @cm, @dm, @em]
-  end
-  
-  def identifier
-    all_objects.map(&:floor).join('')
-  end
-  
+
 end
 
 Day11.new.run
