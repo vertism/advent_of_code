939e572c6ce5b723a104b71724694ca5b99a37cc
Andrew Pepler <andrew@vertism.com>
2016-12-15 14:12:40 +0000

    Make more OO

--- 2016/day11.rb
+++ 2016/day11.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 
 class Day11
-
   def run
     @positions = {}
-    state = [1,1,1,2,3,2,3,2,3,2,3]
+    state = State.new([1,1,1,2,3,2,3,2,3,2,3])
     collect([state,0])
     puts "Part 1: #{@positions['4' * state.size]}"
 
     @positions = {}
-    state = [1,1,1,1,1,1,1,2,3,2,3,2,3,2,3]
+    state = State.new([1,1,1,1,1,1,1,2,3,2,3,2,3,2,3])
     collect([state,0])
     puts "Part 2: #{@positions['4' * state.size]}"
   end
@@ -18,20 +17,16 @@ class Day11
     stack = [start]
     while !stack.empty?
       state, steps = stack.delete_at(0)
-
-      groups(state).each do |objects|
-        occupants = *objects
+      state.groups.each do |objects|
         directions = []
-        directions << 1 if state[0] < 4
-        directions << -1 if state[0] > 1
+        directions << 1 if state.elevator < 4
+        directions << -1 if state.elevator > 1
 
         directions.each do |i|
           new_state = state.clone
-          if move(i, new_state, occupants, steps)
-            if new_state.all? { |i| i == 4 }
-            else
-              stack << [new_state, steps+1]
-            end
+          occupants = *objects
+          if move(i, new_state, occupants, steps) && !new_state.goal?
+            stack << [new_state, steps+1]
           end
         end
       end
@@ -39,20 +34,12 @@ class Day11
     end
   end
 
-  def groups(state)
-    elevator = state[0]
-    available = state.each_index.select{|i| state[i] == elevator }[1..-1]
-    pairs = available.permutation(2).map(&:sort).uniq
-    available + pairs
-  end
-
   def move(direction, state, occupants, steps)
     ([0] + occupants).each { |i| state[i] += direction }
 
-    if valid = valid_move?(state)
-      identifier = hash(state)
-      if !@positions[identifier] || @positions[identifier] > steps+1
-        @positions[identifier] = steps+1
+    if valid = state.valid?
+      if !@positions[state.hash] || @positions[state.hash] > steps+1
+        @positions[state.hash] = steps+1
       else
         valid = false
       end
@@ -60,23 +47,37 @@ class Day11
 
     valid
   end
+end
 
-  def valid_move?(state)
-    pairs = pairs(state)
+class State < Array
+  def elevator
+    self[0]
+  end
+
+  def hash
+    ([elevator] + pairs.flatten).join
+  end
+
+  def pairs
+    self[1..-1].each_slice(2).to_a.sort
+  end
+
+  def groups
+    available = self.each_index.select{|i| self[i] == elevator }[1..-1]
+    pairs = available.permutation(2).map(&:sort).uniq
+    available + pairs
+  end
+
+  def goal?
+    self.all? { |i| i == 4 }
+  end
+
+  def valid?
     pairs.all? do |pair|
       pair[0] == pair[1] ||
       !pairs.map { |p| p[0] }.include?(pair[1])
     end
   end
-
-  def hash(state)
-    ([state[0]] + pairs(state).flatten).join
-  end
-
-  def pairs(state)
-    state[1..-1].each_slice(2).to_a.sort
-  end
-
 end
 
 Day11.new.run
