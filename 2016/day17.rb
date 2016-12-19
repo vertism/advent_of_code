f9e9c339e175a3d15cd2b3cb79521c4d6b3f061d
Andrew Pepler <andrew@vertism.com>
2016-12-19 14:25:15 +0000

    Day 17

--- 2016/day17.rb
+++ 2016/day17.rb
@@ -5,19 +5,50 @@ require 'digest'
 class Day17
 
   def run
-    md5 = Digest::MD5.new
-    @position = "00"
-    @steps = ""
-    @password = "ihgpwlah"
-    while true
+    @md5 = Digest::MD5.new
+    @steps = nil
+    @password = "yjjvjgan"
 
+    move("", "00")
+    puts "Part 1: #{@steps}"
+
+    @steps = nil
+    move("", "00", true)
+    puts "Part 2: #{@steps.size}"
+  end
+
+  def move(steps, position, longest=false)
+    return if !longest && @steps && steps.size > @steps.size
+
+    position = update_position(position, steps.chars.last)
+    hash = @md5.hexdigest("#{@password}#{steps}")[0,4]
+
+    loc = Location.new(hash, position)
+
+    if loc.complete?
+      @steps = steps unless longest
+      @steps = steps if longest && (@steps.nil? || steps.size > @steps.size)
+      return
+    end
+
+    Location.new(hash, position).directions.each do |d|
+      move("#{steps}#{d}", position, longest)
     end
   end
 
-  def move(direction)
-    input = "#{password}#{steps}"
-    hash = md5.hexdigest(input)[0,4]
-    directions = Location.new(hash, position).directions
+  def update_position(position, direction)
+    case direction
+    when 'D'
+      "#{position[0]}#{position[1].to_i + 1}"
+    when 'R'
+      "#{position[0].to_i + 1}#{position[1]}"
+    when 'L'
+      "#{position[0].to_i - 1}#{position[1]}"
+    when 'U'
+      "#{position[0]}#{position[1].to_i - 1}"
+    else
+      position
+    end
   end
 end
 
@@ -30,31 +61,31 @@ class Location
 
   def directions
     unlocked = []
-    unlocked << 'D' if down && can_go_down?
-    unlocked << 'R' if right && can_go_right?
-    unlocked << 'L' if left && can_go_left?
-    unlocked << 'U' if up && can_go_up?
+    unlocked << 'D' if down
+    unlocked << 'R' if right
+    unlocked << 'L' if left
+    unlocked << 'U' if up
     unlocked
   end
 
+  def complete?
+    @position == "33"
+  end
+
   def up
-    char = @hash[0]
-    open?(char) && @position[1] > 0
+    open?(@hash[0]) && @position[1].to_i > 0
   end
 
   def down
-    char = @hash[1]
-    open?(char) && @position[1] < 3
+    open?(@hash[1]) && @position[1].to_i < 3
   end
 
   def left
-    char = @hash[2]
-    open?(char) && @position[0] > 0
+    open?(@hash[2]) && @position[0].to_i > 0
   end
 
   def right
-    char = @hash[3]
-    open?(char) && @position[0] < 3
+    open?(@hash[3]) && @position[0].to_i < 3
   end
 
   def open?(char)
