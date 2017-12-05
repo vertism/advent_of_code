0eb2db8f7deeed114b1936005b3401fd3d98a10d
Andrew Pepler <andrew@vertism.com>
2017-12-05 09:54:01 +0000

    Day 3 part WIP

--- 2017/day3.rb
+++ 2017/day3.rb
@@ -3,10 +3,10 @@
 class Day3
   INPUT = 265149
 
-  def run
+  def run1
     a = 1
     i = 1
-    while a <= INPUT
+    while a < INPUT
       a += 8 * i
       i += 1
     end
@@ -17,6 +17,57 @@ class Day3
     y_offset = numbers_on_side / 2
     x_offset + y_offset
   end
+
+  def run2
+    i = 1
+    @numbers = [1]
+    while value <= INPUT
+      i += 1
+      value = closest(i).sum
+      @numbers << value
+
+    end
+
+    value
+  end
+
+  def closest(i)
+    result = [i-1]
+    if i < 10
+      result << 1
+    end
+
+    if i == 4
+      result << 2
+    elsif i == 6
+      result << 4
+    elsif i == 8
+      result << [2,6]
+    elsif i == 9
+      result << 2
+    elsif i == 10
+      result << 2
+    elsif i == 11
+      result << [2, 3, 9]
+    elsif i == 12
+      result << [2,3]
+    elsif i == 13
+      result << 3
+    elsif i == 14
+      result << [3,4,12]
+    elsif i == 15
+      result << [3,4,5]
+    elsif i == 16
+      result << [4,5]
+    elsif i == 17
+      result << 5
+    elsif i == 18
+      result << [5,6,16]
+    end
+
+    result.flatten
+  end
 end
 
-p Day3.new.run
+p Day3.new.run1
+p Day3.new.run2
