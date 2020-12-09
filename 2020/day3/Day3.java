9f925f19903c3884dd4b1f94231e5a5a1f85cfa4
Andrew Pepler <andrew@vertism.com>
2020-12-09 11:09:22 +0000

    part 2

--- 2020/day3/Day3.java
+++ 2020/day3/Day3.java
@@ -10,7 +10,8 @@ public class Day3 {
 
     List<String> lines = Utils.fileToArray(filename);
     Day3 day3 = new Day3(lines);
-    day3.runPart1();
+    // day3.runPart1();
+    day3.runPart2();
   }
 
   List<String> values;
@@ -38,5 +39,47 @@ public class Day3 {
     }
 
     System.out.println(trees);
+  }
+
+  public void runPart2() {
+    int[] slopeRight = { 1, 3, 5, 7, 1 };
+    int[] slopeDown = { 1, 1, 1, 1, 2 };
+    int[] trees = { 0, 0, 0, 0, 0 };
+
+    for (int i = 0; i < slopeRight.length; i++) {
+      int right = slopeRight[i];
+      int down = slopeDown[i];
+
+      int col = 0;
+      int row = 0;
+
+      for (String val : this.values) {
+        if (row == 0 || row % down != 0) {
+          row++;
+          continue;
+        }
+
+        row++;
+        col = (col + right) % val.length();
+
+        if (val.substring(col, col + 1).equals("#")) {
+          int current = trees[i];
+          trees[i] = current + 1;
+        }
+      }
+    }
+
+    long totalTrees = 0;
+
+    for (long val : trees) {
+
+      if (totalTrees == 0) {
+        totalTrees = val;
+      } else {
+        totalTrees *= val;
+      }
+    }
+
+    System.out.println(totalTrees);
   }
 }
