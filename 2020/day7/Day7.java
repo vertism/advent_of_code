adabae9a724e559902a4de50afab57e20430f632
Andrew Pepler <andrew@vertism.com>
2020-12-09 15:40:18 +0000

    Day 7 part 1

---
+++ 2020/day7/Day7.java
@@ -0,0 +1,58 @@
+package day7;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import utils.Utils;
+
+public class Day7 {
+  public static void main(String[] args) {
+    String filename = "./2020/Day7/input";
+
+    List<String> lines = Utils.fileToArray(filename);
+    Day7 day7 = new Day7(lines);
+    day7.runPart1();
+  }
+
+  List<String> values;
+
+  Day7(List<String> values) {
+    this.values = values;
+  }
+
+  public void runPart1() {
+    Boolean bagsIncreasing = true;
+    Set<String> bags = new HashSet<>();
+    Set<String> moreBags = new HashSet<>();
+    bags.addAll(this.bagsContaining("shiny gold"));
+
+    while (bagsIncreasing) {
+      int currentCount = bags.size();
+      moreBags = new HashSet<>();
+      for (String bag : bags) {
+        moreBags.addAll(bagsContaining(bag.trim()));
+      }
+      bags.addAll(moreBags);
+      if (bags.size() == currentCount) {
+        bagsIncreasing = false;
+      }
+    }
+
+    System.out.println(bags.size());
+  }
+
+  private List<String> bagsContaining(String type) {
+    List<String> bags = new ArrayList<>();
+    for (String line : this.values) {
+      String[] parts = line.split("contain");
+      if (parts[1].contains(type)) {
+        String bag = parts[0].replace("bags", "");
+        bags.add(bag);
+      }
+    }
+
+    return bags;
+  }
+}
