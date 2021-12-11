bf1f6ebc4825f12fb3a7451271052e01a0eb2db5
Andrew Pepler <andrew@vertism.com>
2021-12-11 10:35:55 +0000

    2021 Day 11

---
+++ 2021/Day11/part1.rb
@@ -0,0 +1,83 @@
+class Day11
+  attr_accessor :octopi, :flashes, :ready_to_flash, :flash_count
+
+  def run
+    @octopi = File.open('input').readlines.map(&:strip).map do |line|
+      line.chars.map(&:to_i)
+    end
+
+    @ready_to_flash = []
+
+    @flash_count = 0
+
+    print
+
+    1.upto(100).each do |step|
+      @ready_to_flash = []
+      increment
+      ready_to_flash.each do |point|
+        flash(*point)
+      end
+      reset_octopi
+    end
+
+    p flash_count
+  end
+
+  def print
+    octopi.each do |row|
+      p row
+    end
+    puts
+  end
+
+  def increment
+    octopi.each_with_index do |row, x|
+      row.each_with_index do |_, y|
+        octopi[x][y] += 1
+        if octopi[x][y] > 9
+          ready_to_flash << [x,y]
+        end
+      end
+    end
+  end
+
+  def flash(x, y)
+    octopi[x][y] = "*"
+    increment_point(x-1, y-1)
+    increment_point(x-1, y)
+    increment_point(x-1, y+1)
+    increment_point(x, y-1)
+    increment_point(x, y+1)
+    increment_point(x+1, y-1)
+    increment_point(x+1, y)
+    increment_point(x+1, y+1)
+  end
+
+  def increment_point(x, y)
+    return if octopi[x].nil? || x < 0 || y < 0
+
+    if value = octopi[x][y]
+      if value != "*"
+        octopi[x][y] += 1
+        if value == 9
+          ready_to_flash << [x,y]
+        end
+      end
+    end
+  end
+
+  def reset_octopi
+    octopi.each_with_index do |row, x|
+      row.each_with_index do |octopus, y|
+        if octopus == "*"
+          @flash_count += 1
+          octopi[x][y] = 0
+        end
+      end
+    end
+  end
+
+end
+
+Day11.new.run
