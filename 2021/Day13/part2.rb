83a74e78e89d303a39a8a24e6d764b6a1b58daba
Andrew Pepler <andrew@vertism.com>
2021-12-13 10:43:46 +0000

    2021 Day 13

---
+++ 2021/Day13/part2.rb
@@ -0,0 +1,77 @@
+input = File.open('input').readlines.map(&:strip)
+
+xs = []
+ys = []
+
+input.map do |line|
+  y, x = line.split(',')
+  xs << x.to_i
+  ys << y.to_i
+end
+
+cave_map = Array.new(xs.max + 1) { Array.new(ys.max + 1, " ") }
+
+input.each do |line|
+  next if line.size == 0
+
+  if line.start_with?("fold")
+    if index = line.index("y")
+      fold_direction = "y"
+      fold_location = line[index+2..-1].to_i
+    elsif index = line.index("x")
+      fold_direction = "x"
+      fold_location = line[index+2..-1].to_i
+    end
+
+    if fold_direction == "y"
+      cave_map.each_with_index do |row, y|
+        next if y < fold_location
+
+        if y == fold_location
+          row.each_with_index do |_, x|
+            cave_map[y][x] = " "
+          end
+          next
+        end
+
+        row.each_with_index do |point, x|
+          if point == "#"
+            cave_map[y][x] = " "
+            mirror_location = fold_location - (y - fold_location)
+            cave_map[mirror_location][x] = "#"
+          end
+        end
+      end
+
+      cave_map.delete_if.with_index { |row, y| y >= fold_location }
+    else
+      cave_map.each_with_index do |row, y|
+        row.each_with_index do |point, x|
+          next if x < fold_location
+
+          if x == fold_location
+            cave_map[y][x] = " "
+            next
+          end
+
+          if point == "#"
+            cave_map[y][x] = " "
+            mirror_location = fold_location - (x - fold_location)
+            cave_map[y][mirror_location] = "#"
+          end
+        end
+      end
+
+      cave_map.each_with_index do |row, y|
+        row.delete_if.with_index { |_, x| x >= fold_location }
+      end
+    end
+  else
+    x, y = line.split(',')
+    cave_map[y.to_i][x.to_i] = "#"
+  end
+end
+
+cave_map.each do |row|
+  p row
+end
